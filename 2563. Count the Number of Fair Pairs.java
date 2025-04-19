70b4dc36f27d8d3c88d461a37454b7bcb98210e5
Suvigya Rai <rai.suvigya3@gmail.com>
2025-04-19 18:26:30 +0530

    Time complexity: O(n log n), Space complexity: O(1)

---
+++ 2563. Count the Number of Fair Pairs.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return countLess(nums, upper) - countLess(nums, lower - 1);
+    }
+
+    private long countLess(int[] nums, int sum) {
+        long result = 0;
+        for (int i = 0, j = nums.length - 1; i < j; ++i) {
+            while (i < j && nums[i] + nums[j] > sum)
+                --j;
+            result += j - i;
+        }
+        return result;
+    }
+}
