1c6ca7af5a1993898454a411c78b7a265787f8c1
Suvigya Rai <rai.suvigya3@gmail.com>
2025-04-26 19:08:41 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 2444. Count Subarrays With Fixed Bounds.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+        long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+            long long ans = 0;
+            int minPos = -1, maxPos = -1, leftBound = -1;
+            
+            for (int i = 0; i < nums.size(); ++i) {
+                if (nums[i] < minK || nums[i] > maxK) leftBound = i;
+                if (nums[i] == minK) minPos = i;
+                if (nums[i] == maxK) maxPos = i;
+                ans += max(0, min(minPos, maxPos) - leftBound);
+            }
+            
+            return ans;
+        }
+    };
+    
