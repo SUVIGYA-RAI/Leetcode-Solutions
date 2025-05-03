85e9e51382afda650da5b6aee3bbdc205d7d8176
Suvigya Rai <rai.suvigya3@gmail.com>
2025-05-03 23:15:53 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 1007. Minimum Domino Rotations For Equal Row.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int rotations = check(tops[0], tops, bottoms);
+        if (rotations != -1) return rotations;
+        return check(bottoms[0], tops, bottoms);
+    }
+
+    private int check(int target, int[] tops, int[] bottoms) {
+        int rotateTop = 0, rotateBottom = 0;
+        for (int i = 0; i < tops.length; i++) {
+            if (tops[i] != target && bottoms[i] != target) return -1;
+            else if (tops[i] != target) rotateTop++;
+            else if (bottoms[i] != target) rotateBottom++;
+        }
+        return Math.min(rotateTop, rotateBottom);
+    }
+}
