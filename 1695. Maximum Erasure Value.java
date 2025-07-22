0be785f6d3b2fe63edc014bba68ce52553c7baaf
Suvigya Rai <rai.suvigya3@gmail.com>
2025-07-22 21:59:18 +0530

    Time complexity: O(n), Space complexity: o(n)

---
+++ 1695. Maximum Erasure Value.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int left = 0, right = 0, sum = 0, maxSum = 0;
+        
+        while (right < nums.length) {
+            if (!set.contains(nums[right])) {
+                set.add(nums[right]);
+                sum += nums[right];
+                maxSum = Math.max(maxSum, sum);
+                right++;
+            } else {
+                set.remove(nums[left]);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        
+        return maxSum;
+    }
+}
