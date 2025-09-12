ddb103326c1826b6a488e5adec6f400fd54355c8
Suvigya Rai <rai.suvigya3@gmail.com>
2025-09-12 22:39:09 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ 3227. Vowels Game in a String.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        return ranges::any_of(s, [](char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        });
+    }
+};
