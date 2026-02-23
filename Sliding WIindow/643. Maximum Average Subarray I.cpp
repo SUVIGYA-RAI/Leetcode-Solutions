7fbaf01f99b835b1333a699fed9f80f721d9774d
Suvigya Rai <rai.suvigya3@gmail.com>
2026-02-23 23:40:15 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ Sliding WIindow/643. Maximum Average Subarray I.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        int windowSum = 0;
+        
+        for(int i = 0; i < k; i++) {
+            windowSum += nums[i];
+        }
+        
+        int maxSum = windowSum;
+        
+        for(int i = k; i < n; i++) {
+            windowSum += nums[i] - nums[i - k];
+            maxSum = max(maxSum, windowSum);
+        }
+        
+        return (double)maxSum / k;
+    }
+};
