c7300f0be9507cb216634fee843a33f1d1ff57a1
Suvigya Rai <rai.suvigya3@gmail.com>
2026-03-09 23:30:28 +0530

    Time complexity: O(n), Space complexity: O(1)

---
+++ Arrays/485. Max Consecutive Ones.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int mx = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i]==1) count++; 
+            else count=0;
+            if(count > mx) {
+                mx = count;
+                mx = max(count,mx);
+            }
+        }
+        return mx;
+    }
+};
