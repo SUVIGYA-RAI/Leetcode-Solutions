8818a7c716fc564a3d5654a3615f60fcdb2afef4
Suvigya Rai <rai.suvigya3@gmail.com>
2026-03-15 11:18:21 +0530

    Time complexity O(m log m + n log n), Space complexity: O(1)

---
+++ Arrays/455. Assign Cookies.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int m = g.size();
+        int n = s.size();
+        int i=0, j=0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        while(i<m && j<n) {
+            if(g[i] <= s[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i;
+    }
+};
